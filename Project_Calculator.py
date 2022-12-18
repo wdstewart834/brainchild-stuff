fe1ca320d66a0fa7e61f5768db87c719973d48f2
wdstewart834 <73966696+wdstewart834@users.noreply.github.com>
2022-12-18 14:25:30 -0600

    Add files via upload

---
+++ Project_Calculator.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+from tkinter import ttk
+
+window = tk.Tk()
+window.title("Calculator Program")
+window.geometry("400x360")
+window.configure(bg = '#000000')
+
+window.mainloop()
